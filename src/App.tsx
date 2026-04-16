d9593f46c41c3d3c5fe680fa8102f416602be93b
amolwakhare-einfochips <amol.wakhare@einfochips.com>
2026-04-16 15:33:34 +0530

    added error Boundry

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Week1CatalogPage from "./assignments/week1/pages/Week1CatalogPage";
 import CatalogExplorerPage from "./assignments/week3/pages/CatalogExplorerPage";
+import ErrorBoundary from "./shared/ui/ErrorBoundary";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/week1/catalog" />} />
-      <Route path="/week1/catalog" element={<Week1CatalogPage />} />
-      <Route
-        path="/week3/catalog-explorer"
-        element={<CatalogExplorerPage />}
-      />
-    </Routes>
+    <ErrorBoundary>
+      <Routes>
+        <Route path="/" element={<Navigate to="/week1/catalog" />} />
+        <Route path="/week1/catalog" element={<Week1CatalogPage />} />
+        <Route
+          path="/week3/catalog-explorer"
+          element={<CatalogExplorerPage />}
+        />
+      </Routes>
+    </ErrorBoundary>
   );
 }
 
